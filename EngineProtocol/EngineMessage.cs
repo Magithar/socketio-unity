07a027a142d0886fa670f91feb8e271c27942d0b
Magithar <magithar8@gmail.com>
2026-01-03 00:47:32 +0530

    feat: add Engine.IO message primitives

--- EngineProtocol/EngineMessage.cs
+++ EngineProtocol/EngineMessage.cs
@@ -1,1 +1,24 @@
+namespace SocketIOUnity.EngineProtocol
+{
+    public enum EngineMessageType
+    {
+        Open = 0,
+        Close = 1,
+        Ping = 2,
+        Pong = 3,
+        Message = 4
+    }
+
+    public struct EngineMessage
+    {
+        public EngineMessageType Type;
+        public string Payload;
+
+        public EngineMessage(EngineMessageType type, string payload = null)
+        {
+            Type = type;
+            Payload = payload;
+        }
+    }
+}
 
