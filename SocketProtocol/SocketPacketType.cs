8461ce5827c83f79f7a456e22639be2683f3f48d
Magithar <magithar8@gmail.com>
2026-01-03 00:59:31 +0530

    feat: add Socket.IO packet type definitions

--- SocketProtocol/SocketPacketType.cs
+++ SocketProtocol/SocketPacketType.cs
@@ -1,1 +1,14 @@
+namespace SocketIOUnity.SocketProtocol
+{
+    public enum SocketPacketType
+    {
+        Connect = 0,
+        Disconnect = 1,
+        Event = 2,
+        Ack = 3,
+        Error = 4,
+        BinaryEvent = 5,
+        BinaryAck = 6
+    }
+}
 
