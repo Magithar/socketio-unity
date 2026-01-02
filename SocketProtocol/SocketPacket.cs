97e458819d181c898cb7cf56b7efab07021bf108
Magithar <magithar8@gmail.com>
2026-01-03 01:00:19 +0530

    feat: add Socket.IO packet data model

--- SocketProtocol/SocketPacket.cs
+++ SocketProtocol/SocketPacket.cs
@@ -1,1 +1,23 @@
+namespace SocketIOUnity.SocketProtocol
+{
+    public class SocketPacket
+    {
+        public SocketPacketType Type;
+        public string Namespace;
+        public int? AckId;
+        public string JsonPayload;
+
+        public SocketPacket(
+            SocketPacketType type,
+            string ns = "/",
+            int? ackId = null,
+            string jsonPayload = null)
+        {
+            Type = type;
+            Namespace = ns;
+            AckId = ackId;
+            JsonPayload = jsonPayload;
+        }
+    }
+}
 
