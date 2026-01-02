2887f79b2bf4ffe464c66df3c2ca773c56ef47c6
Magithar <magithar8@gmail.com>
2026-01-03 01:03:14 +0530

    feat: add basic Socket.IO client runtime

--- Runtime/SocketIOClient.cs
+++ Runtime/SocketIOClient.cs
@@ -1,1 +1,48 @@
+using SocketIOUnity.EngineProtocol;
+using SocketIOUnity.SocketProtocol;
+using SocketIOUnity.Transport;
+using System;
+
+namespace SocketIOUnity.Runtime
+{
+    public class SocketIOClient
+    {
+        private readonly EngineIOClient _engine;
+
+        public event Action<string> OnConnect;
+        public event Action<string> OnDisconnect;
+        public event Action<string, string> OnEvent;
+
+        public SocketIOClient(ITransport transport)
+        {
+            _engine = new EngineIOClient(transport);
+            _engine.OnMessage += HandleEngineMessage;
+        }
+
+        public void Connect(string url)
+        {
+            _engine.Connect(url);
+        }
+
+        private void HandleEngineMessage(string raw)
+        {
+            var packet = SocketPacketParser.Parse(raw);
+
+            switch (packet.Type)
+            {
+                case SocketPacketType.Connect:
+                    OnConnect?.Invoke(packet.Namespace);
+                    break;
+
+                case SocketPacketType.Event:
+                    OnEvent?.Invoke(packet.Namespace, packet.JsonPayload);
+                    break;
+
+                case SocketPacketType.Disconnect:
+                    OnDisconnect?.Invoke(packet.Namespace);
+                    break;
+            }
+        }
+    }
+}
 
