c83e5dc41878bc7c7c58b6ba174c98a2e3886e8d
Magithar <magithar8@gmail.com>
2026-01-03 00:39:34 +0530

    Update ITransport.cs

--- Transport/ITransport.cs
+++ Transport/ITransport.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace SocketIOUnity.Transport
+{
+    public interface ITransport
+    {
+        void Connect(string url);
+        void SendText(string message);
+        void SendBinary(byte[] data);
+        void Close();
+
+        event Action OnOpen;
+        event Action OnClose;
+        event Action<string> OnTextMessage;
+        event Action<byte[]> OnBinaryMessage;
+        event Action<string> OnError;
+    }
+}
 
