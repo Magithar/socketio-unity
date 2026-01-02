81d2837b2b47c4181aba73206625a8371627e425
Magithar <magithar8@gmail.com>
2026-01-03 00:44:24 +0530

    feat: add Engine.IO handshake data model

--- EngineProtocol/HandshakeInfo.cs
+++ EngineProtocol/HandshakeInfo.cs
@@ -1,1 +1,11 @@
+namespace SocketIOUnity.EngineProtocol
+{
+    [System.Serializable]
+    public class HandshakeInfo
+    {
+        public string sid;
+        public int pingInterval;
+        public int pingTimeout;
+    }
+}
 
