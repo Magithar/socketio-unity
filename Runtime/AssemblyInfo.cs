29a46c44aa6caba32abbdb22aa6161929c18ae8e
Magithar <magithar8@gmail.com>
2026-02-05 02:00:32 +0530

    chore: expose internal APIs to test assembly

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -2,3 +2,6 @@ using System.Runtime.CompilerServices;
 
 // Expose internal types to Editor assembly for testing
 [assembly: InternalsVisibleTo("SocketIOUnity.Editor")]
+
+// Expose internal types to Test assembly for regression tests (v1.0.1)
+[assembly: InternalsVisibleTo("SocketIOUnity.Tests")]
