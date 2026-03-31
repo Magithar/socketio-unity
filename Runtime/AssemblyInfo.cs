0a127baeb0b7883693c7116b573f782e40c854d7
Magithar <magithar8@gmail.com>
2026-03-31 23:39:09 +0530

    Expose internals to SocketIOUnity.Tests.Stress assembly
    Adds InternalsVisibleTo for the stress test assembly introduced

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -5,3 +5,6 @@ using System.Runtime.CompilerServices;
 
 // Expose internal types to Test assembly for regression tests (v1.0.1)
 [assembly: InternalsVisibleTo("SocketIOUnity.Tests")]
+
+// Expose internal types to Stress Test assembly (v1.3.0)
+[assembly: InternalsVisibleTo("SocketIOUnity.Tests.Stress")]
